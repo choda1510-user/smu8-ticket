77ac0cd0f73fec1f998fced71c26770df5fd6269
jegalhb <jegalhb@gmail.com>
2026-06-12 18:08:56 +0900

    testing jg

---
+++ backend-ticket/src/main/java/com/smu8/ticket/TEST.java
@@ -0,0 +1,10 @@
+package com.smu8.ticket;
+
+public class TEST {
+    static int a = 1;
+    static int b = 2;
+    static int c = a+b;
+    public static void main(String[] args) {
+        System.out.println(c);
+    }
+}

ba837783657824b0d3e46b66923d8039a9b1a0d6
choda1510-smu <choda1510@gmail.com>
2026-06-15 15:03:46 +0900

    Add: AccountHooks.ts 추가

---
+++ frontend/src/hooks/AccountHooks.ts
@@ -0,0 +1,20 @@
+/*
+* crud api를 호출하는 함수를 정의
+* id를 바탕으로 사용자 정보를 불러오는 커스텀 훅 작성
+* 백엔드 url은 항상 .env 파일에서 가져오기
+* 백엔드 구현 전까지는 목업 데이터로 대체
+*/
+function useAccount(id: string|null|undefined) {
+}
+function login() {
+}
+function logout() {
+}
+function signUp() {
+}
+function update() {
+}
+function withdraw() {
+}
+function delete() {
+}

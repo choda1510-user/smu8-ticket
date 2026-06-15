a005bf5dffd3c625f2bdd799050610e9faeda036
choda1510-smu <choda1510@gmail.com>
2026-06-15 15:16:03 +0900

    Chore: 클래스 삭제, 주석 추가

--- backend-ticket/src/main/java/com/smu8/ticket/TicketApplication.java
+++ backend-ticket/src/main/java/com/smu8/ticket/TicketApplication.java
@@ -3,6 +3,10 @@ package com.smu8.ticket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * service 아래는 항상 인터페이스와 인터페이스를 구현한 클래스가 있는 구조로 개발
+ * controller에서는 항상 service 패키지의 인터페이스만 사용하여 의존성 주입을 받음
+ */
 @SpringBootApplication
 public class TicketApplication {
 

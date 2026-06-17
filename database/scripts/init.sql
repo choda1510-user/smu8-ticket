b3b31a19cd08f0609db2b9730efef12dea25db1d
choda1510-smu <choda1510@gmail.com>
2026-06-17 12:26:47 +0900

    Update: 테이블 생성 문 덧붙임
    도커 컴포즈로 적용하려면 smu8-ticket_mysql_database 볼륨과 smu8-ticket-database:latest 이미지를 삭제하고 도커 컴포즈를 실행해야 변경된 스크립트가 적용된다.

--- database/scripts/init.sql
+++ database/scripts/init.sql
@@ -1,3 +1,14 @@
 CREATE DATABASE smu8ticket;
 CREATE USER admin IDENTIFIED BY 'admin';
 GRANT ALL PRIVILEGES ON smu8ticket.* TO 'admin'@'%';
+
+USE smu8ticket;
+
+CREATE TABLE account(
+                        id VARCHAR(36) PRIMARY KEY,
+                        username VARCHAR(256) UNIQUE,
+                        password VARCHAR(512),
+                        nickname VARCHAR(256) UNIQUE,
+                        createdAt DATETIME,
+                        updatedAt DATETIME
+);

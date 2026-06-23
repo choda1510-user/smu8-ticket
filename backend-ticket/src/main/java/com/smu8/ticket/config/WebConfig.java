2ab644e90dbf46caf749379a3d46d835e078a9f2
choda1510-smu <choda1510@gmail.com>
2026-06-23 14:53:02 +0900

    Add: backend cors 설정 분리

--- backend-ticket/src/main/java/com/smu8/ticket/config/WebConfig.java
+++ backend-ticket/src/main/java/com/smu8/ticket/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.smu8.ticket.config;
 import org.jspecify.annotations.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,13 +22,28 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/static/{*path}").addResourceLocations("classpath:/static/");
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/static/swagger-ui/");
     }
+    @Profile("dev")
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer corsDevConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:5173", "http://localhost:80", "http://localhost", "http://192.168.0.6:80", "http://192.168.0.6")
+                        .allowedHeaders("Content-Type", "Authorization")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
+                        .allowCredentials(true);
+            }
+        };
+    }
+    @Profile("test")
+    @Bean
+    public WebMvcConfigurer corsTestConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://192.168.0.6:80", "http://192.168.0.6")
                         .allowedHeaders("Content-Type", "Authorization")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
                         .allowCredentials(true);

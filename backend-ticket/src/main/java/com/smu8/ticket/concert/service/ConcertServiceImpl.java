b00f039d298d740ca1b4a76c08eef864460776f1
hth010719 <hth010719@gmail.com>
2026-06-23 16:42:14 +0900

    병합후 버그 수정완료.

--- backend-ticket/src/main/java/com/smu8/ticket/concert/service/ConcertServiceImpl.java
+++ backend-ticket/src/main/java/com/smu8/ticket/concert/service/ConcertServiceImpl.java
@@ -10,8 +10,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class ConcertServiceImpl implements ConcertService {

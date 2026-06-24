d22aaf86944e0bfd752c93f7333461b7dd434cd9
choda1510-smu <choda1510@gmail.com>
2026-06-24 11:37:27 +0900

    Add: 레디스 설정값 변경 및 추가
    ticket과 file 레디스를 분리

--- backend-ticket/src/main/java/com/smu8/ticket/config/RedisConfig.java
+++ backend-ticket/src/main/java/com/smu8/ticket/config/RedisConfig.java
@@ -4,25 +4,43 @@ package com.smu8.ticket.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
-    @Value("${spring.data.redis.host}")
+    @Value("${spring.data.redis-ticket.host}")
     private String host;
 
-    @Value("${spring.data.redis.port}")
+    @Value("${spring.data.redis-ticket.port}")
     private int port;
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
+    public RedisConnectionFactory redisTicketConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
     }
     @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
+    public RedisTemplate<?, ?> redisTicketTemplate() {
         RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisTicketConnectionFactory());
+        return redisTemplate;
+    }
+    @Profile("dev")
+    @Bean
+    public RedisConnectionFactory redisFileConnectionFactory(
+            @Value("${spring.data.redis-file.host}")
+            String host,
+            @Value("${spring.data.redis-file.port}")
+            int port
+    ) {
+        return new LettuceConnectionFactory(host, port);
+    }
+    @Profile("dev")
+    @Bean
+    public RedisTemplate<?, ?> redisFileTemplate(RedisConnectionFactory redisFileConnectionFactory) {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisFileConnectionFactory);
         return redisTemplate;
     }
 }
